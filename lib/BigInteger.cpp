32f5a8b618ef759729ca2d8aa6354d24c7f797df
Thomas Royer <thomas.royer@blackfire.io>
2016-09-02 18:33:40 +0200

    Fix string conversion

--- lib/BigInteger.cpp
+++ lib/BigInteger.cpp
@@ -27,8 +27,11 @@ string BigInteger::toString() const
     string result;
 
     for (auto it = decomposition.rbegin(); it != decomposition.rend(); ++it) {
-        result += to_string(*it);
+        string part = to_string(*it);
+        result += string(DIGITS_PER_PART - part.size(), '0') + part;
     }
+
+    result.erase(0, result.find_first_not_of('0'));
 
     return result;
 }
--- lib/test/BigInteger.cpp
+++ lib/test/BigInteger.cpp
@@ -10,6 +10,7 @@ int main()
     BEGIN_TEST();
 
     ASSERT_EQUALS(BigInteger("123456789987654321").toString(), "123456789987654321");
+    ASSERT_EQUALS(BigInteger("10000000009").toString(), "10000000009");
     ASSERT_EQUALS((BigInteger("123456") + BigInteger("987987987")).toString(), "988111443");
 
     END_TEST();
